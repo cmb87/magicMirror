89ebfa2578b167f29dc1eea9cef2842bbc506dd5
chr82917 <christian.peeren@iais.fraunhofer.de>
2021-06-10 09:35:01 +0200

    last commit

--- docker/start_grafana-kiosk.sh
+++ docker/start_grafana-kiosk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+docker rm grafana
 docker run -d --name=grafana -v /home/pi/projects/magicMirror/grafana:/var/lib/grafana -p 3000:3000 -e GF_SECURITY_ADMIN_USER=xxx -e GF_SECURITY_ADMIN_PASSWORD=test grafana/grafana
 sleep 10
 /usr/bin/grafana-kiosk --URL http://localhost:3000/playlists/play/1 --login-method local --username xxx --password test --kiosk-mode tv
